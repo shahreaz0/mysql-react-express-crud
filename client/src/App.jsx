fbd312b19b2cf9dee7084e8a8cbe302e2900461f
shahreaz <ashahreaz@gmail.com>
2021-08-19 00:50:02 +0600

    Add Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,12 @@ import { Switch, Route, NavLink } from "react-router-dom";
 import "./App.css";
 import EmployeeForm from "./components/EmployeeForm";
 import EmployeeList from "./components/EmployeeList";
+import Navbar from "./components/Navbar";
 
 const App = (props) => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink exact to="/employees">
-              All Employees
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-
+      <Navbar />
       <Switch>
         <Route exact path="/" component={EmployeeForm} />
         <Route exact path="/employees" component={EmployeeList} />
